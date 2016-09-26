cad20c8c892955e8016b8b8755923ace1baa024c
Mark Griffiths <mark@thebespokepixel.com>
2016-09-26 10:29:21 +0100

    Ensure named colors use Hex3 notation consistently

--- index.js
+++ index.js
@@ -1255,7 +1255,7 @@ const names = {
 	plum: 'dda0dd',
 	powderblue: 'b0e0e6',
 	purple: '800080',
-	rebeccapurple: '663399',
+	rebeccapurple: '639',
 	red: 'f00',
 	rosybrown: 'bc8f8f',
 	royalblue: '4169e1',
