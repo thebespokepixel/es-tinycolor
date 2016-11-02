fef0634fceb1db4596cd248da0882ad705b07d99
Mark Griffiths <mark@thebespokepixel.com>
2016-11-02 10:08:44 +0000

    Update deps

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,12 @@ gulp.task('bundle', cordial.macro({
 	},
 	exports: 'named',
 	source: 'src/index.js'
-}).basic())
+}).bundle())
+
+// Clean
+gulp.task('clean', cordial.shell({
+	source: ['lib', 'npm-debug.*', './.nyc_output', './coverage']
+}).trash())
 
 // Tests
 gulp.task('ava', cordial.test().ava(['test/*.js']))
@@ -15,9 +20,9 @@ gulp.task('xo', cordial.test().xo(['src/*.js']))
 gulp.task('test', gulp.parallel('xo', 'ava'))
 
 // Hooks
-gulp.task('start-release', gulp.series('reset', 'bundle'))
+gulp.task('start-release', gulp.series('reset', 'clean', 'bundle'))
 gulp.task('test-release', gulp.series('test'))
 gulp.task('finish-release', gulp.series('push-force', 'push-tags'))
 
 // Default
-gulp.task('default', gulp.series('bump', 'bundle'))
+gulp.task('default', gulp.series('bump', 'clean', 'bundle'))
